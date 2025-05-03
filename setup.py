2e6d30adb25b7d0772753e923b3a40d9a5b508c3
shilpa07-ingole <shilpa.ingole@gmail.com>
2025-05-03 21:36:00 +0530

    added folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(name="e-commerce-bot",
+      version="0.0.1",
+      author="shilpa",
+      author_email="shilpa.ingole@gmail.com",
+      packages=find_packages(),
+      install_requires=['lancedb','langchain','langgraph','tavily-python','polygon']
+
+
+
